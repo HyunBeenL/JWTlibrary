34b7351319bbce39e5bfc79e52d653b8dbf8b89e
hyunbeen <leehv1234@kookmin.ac.kr>
2025-09-27 15:20:07 +0900

    초기 설정

---
+++ src/test/java/org/project/jwtlibrary/JwtlibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package org.project.jwtlibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtlibraryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
